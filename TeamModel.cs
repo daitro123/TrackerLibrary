7a460d5f642da86509ede777a5c6af16a8eba47c
Tomáš Macek <tomas.macek@igalileo.cz>
2022-09-18 20:40:47 +0200

    Add project files.

---
+++ TeamModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TrackerLibrary
+{
+    public class TeamModel
+    {
+        public List<Person> TeamMembers { get; set; } = new List<Person>;
+        public string TeamName { get; set; }
+
+    }
+}
